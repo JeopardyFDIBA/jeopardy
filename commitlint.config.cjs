f2c6346a6d16f523e0e7525d90b46b1b8b2376c3
Aleksey12SV <aleksey12sv@gmail.com>
2023-02-22 00:14:08 +0200

    feat(jeopardy-FE)- Prepare husky

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,7 +1,8 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-      'subject-case': [2, 'always', 'sentence-case'],
-      'header-max-length': [2, 'always', 120],
-    },
-  };
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'subject-case': [2, 'always', 'sentence-case'],
+    'header-max-length': [2, 'always', 120],
+  },
+};
