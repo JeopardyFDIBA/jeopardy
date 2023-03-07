1b2ced7b17412a7fb4cd77c02d4536d081286097
Aleksey12SV <aleksey12sv@gmail.com>
2023-03-07 10:58:48 +0200

    feat: Add Router

--- jeopardy-FE/src/App.tsx
+++ jeopardy-FE/src/App.tsx
@@ -1,15 +1,18 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Main from './components/Main';
 import UsersList from './components/UsersList';
 
 function App() {
   return (
-    <div>
-      <Header title="Welcome to Jeopardy" />
-      <UsersList />
-      <Main />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Header title="Welcome to Jeopardy" />} />
+        <Route path="/test" element={<UsersList />} />
+        <Route path="/game" element={<Main />} />
+      </Routes>
+    </Router>
   );
 }
 
