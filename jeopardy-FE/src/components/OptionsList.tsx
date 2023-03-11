9cae2f54268b1428b44517e006d9e7319bdfe710
Aleksey12SV <aleksey12sv@gmail.com>
2023-03-11 15:25:13 +0200

    feat: add and style OptionsList

---
+++ jeopardy-FE/src/components/OptionsList.tsx
@@ -0,0 +1,14 @@
+import OptionsItem from './OptionsItem';
+import styles from './OptionsList.module.scss';
+
+function OptionsList() {
+  return (
+    <div className={styles.optionsContainer}>
+      <OptionsItem destination="/highscores" title="Highscores" />
+      <OptionsItem destination="/game" title="Get Started" />
+      <OptionsItem destination="/playground" title="Playground" />
+    </div>
+  );
+}
+
+export default OptionsList;
