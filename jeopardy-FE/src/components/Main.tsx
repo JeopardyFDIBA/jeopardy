cac2bd6478af15861b5b637a0776322d41539750
Aleksey12SV <aleksey12sv@gmail.com>
2023-03-14 00:07:12 +0200

    feat: Add custom notification

--- jeopardy-FE/src/components/Main.tsx
+++ jeopardy-FE/src/components/Main.tsx
@@ -4,10 +4,13 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable react/no-array-index-key */
 import { useEffect, useRef, useState } from 'react';
-import toast, { Toaster } from 'react-hot-toast';
+import { ToastContainer, toast } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
 import { data } from '../mockdata';
 import styles from './Main.module.scss';
 import Column from './Column';
+import Avatar from 'react-avatar';
 
 export default function Main() {
   const ref = useRef(null);
@@ -19,20 +22,19 @@ export default function Main() {
   const categories: string[] = Object.keys(data);
   const [buzzer, setBuzzer] = useState('');
 
+  const notify = () => toast.success("Clicked first", {
+    icon: ({theme, type}) =>  <Avatar round name="Aleksey Svistunov" />
+  });
+  
+
   const handleKeyDown = (event:any) => {
-    toast.success('this is notification');
-    // switch (event.key) {
-    //   case '1': setBuzzer('#a62a21'); alert(message); break;
-    //   case '2': setBuzzer('#b3003c'); alert(message); break;
-    //   case '3': setBuzzer('#3a6024'); alert(message); break;
-    //   case '4': setBuzzer('#0b51c1'); alert(message); break;
-    //   default: break;
-    // }
+    notify();
+    toast.clearWaitingQueue();
   };
 
   return (
     <div className={styles.wrapper}>
-      <Toaster />
+       <ToastContainer limit={1} />
       <div className={styles.questionsField} style={{ backgroundColor: buzzer || '#0c0734' }} ref={ref} tabIndex={-1} onKeyDown={handleKeyDown}>
         <div className={styles.questions}>
           {categories.map((elem:string, index: number) => (
