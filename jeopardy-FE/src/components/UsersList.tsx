1fbf8166f070fff40f5696670c5cb6df5c218557
Aleksey Svistunov <aleksey12sv@gmail.com>
2023-04-05 00:23:20 +0300

    feat: Set colors to player avatars

--- jeopardy-FE/src/components/UsersList.tsx
+++ jeopardy-FE/src/components/UsersList.tsx
@@ -1,22 +1,30 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable react/no-array-index-key */
 /* eslint-disable import/no-extraneous-dependencies */
-import React from 'react';
 import Avatar from 'react-avatar';
 import styles from './UsersList.module.scss';
-
-interface IProps {
-  id: number,
-  name: string,
-}
+import colors from '../helpers/colors';
+import players from '../helpers/players';
+import { maxInitials } from '../helpers/helpConstants';
 
 function UsersList() {
-  const players = JSON.parse(localStorage.getItem('players') || 'no players');
   const users: any = [];
-  Object.values(players).forEach((val:any) => users.push(val));
+  const avatarColors: string[] = [];
+  Object.values(players).forEach((val:any, i) => {
+    avatarColors.push(colors[i]);
+    users.push(val);
+  });
+
   return (
     <div className={styles.wrapperList}>
       {users.map((element: { score: number, name: string }, index: number) => (
-        <Avatar key={index} round name={element.name} />
+        <Avatar
+          key={index}
+          round
+          name={element.name}
+          maxInitials={maxInitials}
+          color={avatarColors[index]}
+        />
       ))}
     </div>
   );
