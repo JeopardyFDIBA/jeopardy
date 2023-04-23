776789fdf0227ae99dc691cd6ca5eaee69c841f4
Aleksey12SV <113349663+Aleksey12SV@users.noreply.github.com>
2023-04-24 01:13:32 +0300

    Feat/sonar (#11)
    feat: add Sonar

--- jeopardy-BE/src/test/java/com/jeopardy/JeopardyApplicationTests.java
+++ jeopardy-BE/src/test/java/com/jeopardy/JeopardyApplicationTests.java
@@ -3,11 +3,11 @@ package com.jeopardy;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class JeopardyApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class JeopardyApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
