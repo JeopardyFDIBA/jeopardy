7378b33402c9a86abe0e9f3e5261d2a188974c2f
Aleksey Svistunov <aleksey12sv@gmail.com>
2023-05-07 19:24:21 +0300

    feat: Add tests for Input and Column

--- jeopardy-FE/vite.config.ts
+++ jeopardy-FE/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
-    environment: 'jsdom',
+    environment: 'jest',
     setupFiles: ['./src/setupTests.ts'],
   },
 });
